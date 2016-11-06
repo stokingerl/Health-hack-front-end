b55c41484367ed7b05c6feadb60cc8eb28fd88fc
stokingerl <liamstokinger@gmail.com>
2016-11-05 23:12:47 -0400

    home page shell

--- app/components/home/homeController.js
+++ app/components/home/homeController.js
@@ -1,8 +1,18 @@
 (function() {
     var app = angular.module('routerApp');
 
+    var constants = {
+        lifeGoal: "\"I want to be able to dance at my daughter's wedding.\"",
+        concreteGoal: "Lose 20 pounds",
+        actionText: "Run for 15 minutes",
+        moodText: "How are you feeling today?",
+        todayText: "Today",
+        bpLabel: "Blood pressure:",
+        weightLabel: "Weight:"
+    }
+
     app.controller('homeController', function($scope) {
         var vm = this;
-        vm.text = "Hello World!";
+        vm.constants = constants;
     });
 })();
