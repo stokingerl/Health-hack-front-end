d13db14cf2a10795249c4229d96780eab1c46e60
Peter Masucci <peter.a.masucci@gmail.com>
2016-11-06 00:12:48 -0400

    Add confidence component

--- app/components/confidence/confidenceController.js
+++ app/components/confidence/confidenceController.js
@@ -3,6 +3,11 @@
 
     app.controller('confidenceController', function($scope) {
         var vm = this;
+        vm.slider = new Slider("input.slider", {
+        	min: 0,
+        	max: 5,
+        	step: 1
+        });
         vm.prompt = "How confident are you that you can achieve this goal?";
     });
 })();
