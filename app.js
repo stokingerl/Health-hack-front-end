87bd43693c094583e245847ebdefba3f5d80c552
Adam Zimmer <adamz@slalom.com>
2016-11-05 21:09:19 -0400

    added routing for 3 different pages

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 var routerApp = angular.module('routerApp', ['ui.router']);
 
 routerApp.config(function($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/home');
+    $urlRouterProvider.otherwise('/');
 
     $stateProvider
 
+    .state('signup', {
+            url: '/',
+            templateUrl: 'app/components/signup/Signup.html',
+            controller: 'signupController as vm'
+    })
     .state('home', {
             url: '/home',
             templateUrl: 'app/components/home/homeView.html',
             controller: 'homeController as vm'
+    })
+    .state('dashboard', {
+            url: '/dashboard',
+            templateUrl: 'app/components/dashboard/dashboard.html',
+            controller: 'dashboardController as vm'
     });
 });
